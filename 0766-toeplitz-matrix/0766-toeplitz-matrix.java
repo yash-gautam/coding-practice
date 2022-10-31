b2c7bb085f043ddd112c90822ad30ecc16813fd6
yash-gautam <82763523+yash-gautam@users.noreply.github.com>
2022-10-31 13:07:41 +0530

    Time: 1 ms (93.83%), Space: 45.7 MB (54.67%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -3,17 +3,25 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         
-        HashMap<Integer, Integer> map = new HashMap<>();
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<m; j++){
+//                 int ele = matrix[i][j];
+                
+//                 if(!map.containsKey(i-j)){
+//                     map.put(i-j, ele);
+//                 } else{
+//                     if(ele!=map.get(i-j)){
+//                         return false;
+//                     }
+//                 }
+//             }
+//         }
+        
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                int ele = matrix[i][j];
-                
-                if(!map.containsKey(i-j)){
-                    map.put(i-j, ele);
-                } else{
-                    if(ele!=map.get(i-j)){
-                        return false;
-                    }
+                if(i!=0 && j!=0 && matrix[i][j]!=matrix[i-1][j-1]){
+                    return false;
                 }
             }
         }
