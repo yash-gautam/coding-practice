04a08259725243e8321c2229f8ffb36a6fad39b9
yash-gautam <82763523+yash-gautam@users.noreply.github.com>
2022-11-02 17:04:22 +0530

    Time: 2 ms (85.88%), Space: 43.3 MB (88.16%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -3,24 +3,25 @@ class Solution {
         int n = arr.length;
         if(n<3) return false;
         
-        boolean peak = false;
+        boolean hitPeak = false;
         
         for(int i=1; i<n-1; i++){
             int curr = arr[i];
             int prev = arr[i-1];
             int nxt = arr[i+1];
             
-            if(!peak && curr>prev){
+            if(!hitPeak && curr>prev){
                 if(curr>nxt){
-                    peak = true;
+                    hitPeak = true;
                 }
-            } else if(peak && curr>nxt){
-                
+            } else if(hitPeak && curr>nxt){
+                continue;
             } else {
                 return false;
             }
+            
         }
         
-        return peak ? true:false;
+        return hitPeak ? true:false;
     }
 }
