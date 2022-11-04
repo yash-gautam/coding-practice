bd6b79a9afcfa3eb1855e70d09e7de5b900364c6
yash-gautam <82763523+yash-gautam@users.noreply.github.com>
2022-11-04 11:20:40 +0530

    Time: 4 ms (94.43%), Space: 44.8 MB (66.22%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,25 +1,42 @@
 class Solution {
     public String reverseVowels(String s) {
-        Stack<Character> stk = new Stack<>();
         int n = s.length();
-        StringBuilder sb = new StringBuilder();
+        char[] arr = s.toCharArray();
         
-        for(char ch: s.toCharArray()){
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' 
+        int start = 0;
+        int end = n-1;
+        
+        while(start<end){
+            while(start<end && isVowel(arr[start])==false){
+                start++;
+            }
+            while(start<end && isVowel(arr[end])==false){
+                end--;
+            }
+            
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            start++;
+            end--;            
+        }
+        
+        return String.valueOf(arr);
+    }
+    
+    void print(char[] arr){
+        for(char ch: arr){
+            System.out.print(ch+", ");
+        }
+        System.out.println();
+    }
+    boolean isVowel(char ch){
+        if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' 
                || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
-                stk.push(ch);
-            }
+            return true;
         }
         
-        for(char ch: s.toCharArray()){
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'
-              || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
-                sb.append(stk.pop());
-            } else{
-                sb.append(ch);
-            }
-        }
-        
-        return sb.toString();
+        return false;
     }
 }
