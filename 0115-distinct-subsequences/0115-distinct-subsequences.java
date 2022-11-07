df7047c9d8dc2ec4dcf9310c5cf9e2ef969ffd16
yash-gautam <82763523+yash-gautam@users.noreply.github.com>
2022-11-07 15:32:58 +0530

    Time: 47 ms (14.99%), Space: 49.9 MB (66.62%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -8,21 +8,39 @@ class Solution {
             return 0;
         }
         
-        int[][] dp = new int[n][m];
-        for(int[] arr: dp){
-            Arrays.fill(arr, -1);
+        int[][] dp = new int[n+1][m+1];
+        
+        for(int i=0; i<m; i++){
+            dp[0][i] = 0;
+        }
+        for(int i=0; i<n; i++){
+            dp[i][0] = 1;
         }
         
-        return solve(s, n-1, t, m-1, dp);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                int not_take = 0;
+                int take = 0;
+
+                not_take = dp[i-1][j];
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    take += dp[i-1][j-1];
+                }
+
+                dp[i][j] = take + not_take;
+            }
+        }
+        
+        return dp[n][m];
     }
     
     public int solve(String s, int i, String t, int j, int[][] dp){
                 
-        if(j<0){
+        if(j==0){
             return 1;
         }
         
-        if(i<0){
+        if(i==0){
             return 0;
         }
         
@@ -34,7 +52,7 @@ class Solution {
         int take = 0;
         
         not_take = solve(s, i-1, t, j, dp);
-        if(s.charAt(i)==t.charAt(j)){
+        if(s.charAt(i-1)==t.charAt(j-1)){
             take += solve(s, i-1, t, j-1, dp);
         }
         
