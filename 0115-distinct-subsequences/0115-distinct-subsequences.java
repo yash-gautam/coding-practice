1f2a36a7ba97094483e31b6b757c44bf57d8ae52
yash-gautam <82763523+yash-gautam@users.noreply.github.com>
2022-11-07 15:33:24 +0530

    Time: 46 ms (16.77%), Space: 50.1 MB (50.85%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -9,10 +9,6 @@ class Solution {
         }
         
         int[][] dp = new int[n+1][m+1];
-        
-        for(int i=0; i<m; i++){
-            dp[0][i] = 0;
-        }
         for(int i=0; i<n; i++){
             dp[i][0] = 1;
         }
@@ -32,30 +28,5 @@ class Solution {
         }
         
         return dp[n][m];
-    }
-    
-    public int solve(String s, int i, String t, int j, int[][] dp){
-                
-        if(j==0){
-            return 1;
-        }
-        
-        if(i==0){
-            return 0;
-        }
-        
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        
-        int not_take = 0;
-        int take = 0;
-        
-        not_take = solve(s, i-1, t, j, dp);
-        if(s.charAt(i-1)==t.charAt(j-1)){
-            take += solve(s, i-1, t, j-1, dp);
-        }
-        
-        return dp[i][j] = take + not_take;
     }
 }
