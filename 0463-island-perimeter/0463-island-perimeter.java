3a4500b9e2c3e57ca6128ef3ff390eed0d8957b4
yash-gautam <82763523+yash-gautam@users.noreply.github.com>
2022-11-08 13:05:31 +0530

    Time: 10 ms (81.11%), Space: 61.9 MB (65.68%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -8,31 +8,17 @@ class Solution {
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(grid[i][j]==1){
-                    int[][] vis = new int[n][m];
-                    per = solve(grid, i, j, vis);
-                    break;
+                    per+=4;
+                    if(i>0 && grid[i-1][j]==1){
+                        per-=2;
+                    }
+                    if(j>0 && grid[i][j-1]==1){
+                        per-=2;
+                    }
                 }
             }
         }
         
         return per;
-    }
-    
-    int solve(int[][] grid, int r, int c, int[][] vis){        
-        if(r<0 || c<0 || r>=grid.length || c>=grid[0].length || grid[r][c]==0){
-            return 1;
-        }
-        
-        if(vis[r][c]==1){
-            return 0;
-        }
-        
-        vis[r][c] = 1;
-        int left = solve(grid, r, c-1, vis);
-        int right = solve(grid, r, c+1, vis);
-        int up = solve(grid, r-1, c, vis);
-        int down = solve(grid, r+1, c, vis);
-        
-        return left+right+up+down;
     }
 }
