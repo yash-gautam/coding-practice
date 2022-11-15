7aadb03203566f06c2c85affee78dcbf638b3ec7
yash-gautam <82763523+yash-gautam@users.noreply.github.com>
2022-11-15 18:01:31 +0530

    Time: 0 ms (100.00%), Space: 47 MB (71.47%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -22,31 +22,57 @@ class Node {
 */
 
 class Solution {
+//     public Node connect(Node root) {
+//         if(root==null){
+//             return root; 
+//         }
+        
+//         Queue<Node> q = new LinkedList<>();
+//         q.offer(root);
+        
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             for(int i=0; i<size; i++){
+//                 Node node = q.poll();
+//                 if(i==size-1){
+//                     node.next = null;
+//                 } else{
+//                     node.next = q.peek();
+//                 }
+                
+//                 if(node.left!=null){
+//                     q.add(node.left);
+//                 }
+//                 if(node.right!=null){
+//                     q.add(node.right);
+//                 }
+//             }
+//         }
+        
+//         return root;
+//     }
+    
     public Node connect(Node root) {
         if(root==null){
             return root; 
         }
         
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
+        Node curr = root;
         
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                Node node = q.poll();
-                if(i==size-1){
-                    node.next = null;
+        while(curr.left!=null){
+            Node node = curr;
+            
+            while(true){
+                node.left.next = node.right;
+                if(node.next!=null){
+                    node.right.next = node.next.left;
+                    node = node.next;
                 } else{
-                    node.next = q.peek();
-                }
-                
-                if(node.left!=null){
-                    q.add(node.left);
-                }
-                if(node.right!=null){
-                    q.add(node.right);
+                    break;
                 }
             }
+            
+            curr = curr.left;
         }
         
         return root;
