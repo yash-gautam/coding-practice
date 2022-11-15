1cab14c997efeb2e6405200ce547c293218a4e05
yash-gautam <82763523+yash-gautam@users.noreply.github.com>
2022-11-15 10:51:19 +0530

    Time: 173 ms (9.79%), Space: 94.4 MB (22.34%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        int n = arr.length;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i: arr){
+            map.put(i, map.getOrDefault(i, 0)+1);
+        }
+        
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b)->(map.get(b)-map.get(a)));
+        pq.addAll(map.keySet());
+                
+        int elementsLeft = n;
+        int size = 0;
+        while(!pq.isEmpty()){
+            int top = pq.poll();
+            elementsLeft -= map.get(top);
+            size++;
+            if(elementsLeft<=n/2){
+                break;
+            }
+        }
+        
+        return size;
+    }
+}
