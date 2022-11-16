d3020ab9c12b63a9d837bebea8ed9c7fd6105735
yash-gautam <82763523+yash-gautam@users.noreply.github.com>
2022-11-16 09:07:24 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (99.72%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,26 +14,50 @@
  * }
  */
 class Solution {
+    int leftNode = 0;
+    int maxDepth = 0;
+    
     public int findBottomLeftValue(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-        
-        int leftNode = -1;
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                TreeNode node = q.poll();
-                if(node.right!=null){
-                    q.offer(node.right);
-                }
-                if(node.left!=null){
-                    q.offer(node.left);
-                }
-                
-                leftNode = node.val;
-            }
-        }
-        
+        solve(root, 1);
         return leftNode;
     }
+    
+    public void solve(TreeNode root, int depth){
+        if(root==null){
+            return;
+        }
+        
+        if(depth>maxDepth){
+            maxDepth = depth;
+            leftNode = root.val;
+        }
+        
+        solve(root.left, depth+1);
+        solve(root.right, depth+1);
+        
+    }
+    
+    
+//     public int findBottomLeftValue(TreeNode root) {
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+        
+//         int leftNode = -1;
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             for(int i=0; i<size; i++){
+//                 TreeNode node = q.poll();
+//                 if(node.right!=null){
+//                     q.offer(node.right);
+//                 }
+//                 if(node.left!=null){
+//                     q.offer(node.left);
+//                 }
+                
+//                 leftNode = node.val;
+//             }
+//         }
+        
+//         return leftNode;
+//     }
 }
